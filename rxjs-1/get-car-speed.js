862355077c0e20a82142efc2df376390a46b3171
Paul LOUIS <>
2019-07-03 22:03:04 +0200

    i am speed !

--- rxjs-1/get-car-speed.js
+++ rxjs-1/get-car-speed.js
@@ -4,7 +4,12 @@ const RxOp = require('rxjs/operators');
 function getCarSpeed(race, carName) {
     return Rx.fromEvent(race, 'data').pipe(
         RxOp.filter(data => data.carName === carName),
-        
+        RxOp.bufferTime(200),
+        RxOp.map(values => {
+            var oldest = values[0];
+            var latest = values[values.length - 1];
+            return 1000*(latest.xLocation - oldest.xLocation)/(latest.time - oldest.time);
+        })
   );
 }
 
